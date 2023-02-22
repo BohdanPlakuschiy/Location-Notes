389049d622a9f2db8b5cde5f6ba400b01edcdb1e
Bohdan Plakuschiy <plakushiy.bogdan@gmail.com>
2023-02-22 22:19:02 +0200

    no message

--- LocationNotes/LocationNotes/Model/CoreDataManager.swift
+++ LocationNotes/LocationNotes/Model/CoreDataManager.swift
@@ -6,7 +6,36 @@
 //
 
 import UIKit
+import CoreData
 
-class CoreDataManager: NSObject {
-
+class CoreDataManager {
+    
+    static let sharedInstance = CoreDataManager()
+    
+    var managedObjecContext: NSManagedObjectContext {
+        return persistentContainer.viewContext
+    }
+    
+        lazy var persistentContainer: NSPersistentContainer = {
+            let container = NSPersistentContainer(name: "LocationNotes")
+            container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+                if let error = error as NSError? {
+                    fatalError("Unresolved error \(error), \(error.userInfo)")
+                }
+            })
+            return container
+        }()
+        
+        func saveContext () {
+            let context = persistentContainer.viewContext
+            if context.hasChanges {
+                do {
+                    try context.save()
+                } catch {
+                    let nserror = error as NSError
+                    fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+                }
+            }
+        }
+    
 }
